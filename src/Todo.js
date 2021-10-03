44b465f487c0ecd342750b243b02be3c41a929a3
Dong Li <87606683+eastlime93@users.noreply.github.com>
2021-10-03 11:41:32 -0500

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -1,23 +1,39 @@
 import React from 'react'
 
-export default function Todo({title, description, dateCreated, complete, dateCompleted}){
+export default function Todo({title, description, dateCreated, complete, dateCompleted, user, todoId, dispatchTodos}){
 
-  let tickBox;
-  if(complete){
-    tickBox = <input type="checkbox" id="complete-status" name="complete-status" checked/>
-    }else{
-    tickBox = <input type="checkbox" id="complete-status" name="complete-status"/>
-    }
   return(
     <div>
+
         <h3>{title}</h3>
+
         <div>{description}</div>
         <br />
         <div><i>Date Created: {dateCreated}</i></div>
         <br />
-        <div><i>Complete: {tickBox}</i></div>
+        <div><i>Complete: <input type="checkbox" checked={complete} onClick={e=> dispatchTodos({type:"TOGGLE_TODO", index:todoId, completeCheck:!complete, completedDateVal:completeDateGetter(!complete)})} /></i></div>
         <br />
         <div><i>Date Completed: {dateCompleted}</i></div>
-    </div>
-  )
+        <br />
+        <i>Added by <b>{user}</b></i><br /><br />
+
+        <button onClick={e => dispatchTodos({type:"DELETE_TODO", index:todoId})}>Delete</button>
+
+   </div>
+ )
+}
+
+function completeDateGetter(complete){
+    let currentTime ="";
+    if(complete){
+      const ts = Date.now();
+
+      const date_ob = new Date(ts);
+      const date = date_ob.getDate();
+      const month = date_ob.getMonth() + 1;
+      const year = date_ob.getFullYear();
+
+      currentTime = `${year}-${month}-${date}`;
+  }
+    return currentTime;
 }
