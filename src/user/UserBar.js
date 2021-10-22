5a8964ab4fa13fbd0b18bff46c5cff7fdb93417c
Dong Li <87606683+eastlime93@users.noreply.github.com>
2021-10-22 04:22:52 -0500

    Update UserBar.js

--- src/user/UserBar.js
+++ src/user/UserBar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {StateContext} from '../contexts'
 import Login from './Login'
 import Logout from './Logout'
 import Registration from './Registration'
 
-export default function UserBar({user, dispatchUser}){
-  if(user){
-      return <Logout user={user} dispatchUser={dispatchUser} />
+export default function UserBar(){
+
+  const {state} = useContext(StateContext)
+
+  if(state.user){
+      return <Logout />
     }else{
       return (
         <>
-          <Login dispatchUser={dispatchUser}/>
-          <Registration dispatchUser={dispatchUser}/>
+          <Login />
+          <Registration />
         </>
       )
     }
